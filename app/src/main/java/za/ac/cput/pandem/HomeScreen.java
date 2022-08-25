44be78b47f39b64ec6ed750b2785b87d91cccfdd
ChanteLeeDavids <220246688@mycput.ac.za>
2022-08-25 22:04:49 +0200

    HomeScreen.java modfified

--- app/src/main/java/za/ac/cput/pandem/HomeScreen.java
+++ app/src/main/java/za/ac/cput/pandem/HomeScreen.java
@@ -2,15 +2,31 @@ package za.ac.cput.pandem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import za.ac.cput.pandem.R;
 
 public class HomeScreen extends AppCompatActivity {
 
+    RelativeLayout mapBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        mapBtn =  findViewById(R.id.map_icon_layout);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(HomeScreen.this, );
+//                startActivity(intent);
+            }
+        });
+
     }
 }
