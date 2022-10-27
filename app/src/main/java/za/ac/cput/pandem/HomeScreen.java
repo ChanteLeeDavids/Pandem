705102c3c637dfd1e6132c988a53e85f55dfe1c9
NolubabaloNdongeni <nndongeni89@gmail.com>
2022-10-27 19:00:31 +0200

    Map View added

--- app/src/main/java/za/ac/cput/pandem/HomeScreen.java
+++ app/src/main/java/za/ac/cput/pandem/HomeScreen.java
@@ -2,10 +2,16 @@ package za.ac.cput.pandem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import za.ac.cput.pandem.R;
 
@@ -13,20 +19,49 @@ public class HomeScreen extends AppCompatActivity {
 
     RelativeLayout mapBtn;
 
+    private static final String TAG = "HomeScreen";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        mapBtn =  findViewById(R.id.map_icon_layout);
-
+        if (isServicesOK()){
+            init();
+        }
+    }
+    private void init(){
+        mapBtn = findViewById(R.id.map_icon_layout);
         mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(HomeScreen.this, );
-//                startActivity(intent);
+                Intent intent = new Intent(HomeScreen.this, MapActivity.class);
+                startActivity(intent);
             }
         });
+
+
+    }
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(HomeScreen.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG,"isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG,"isServicesOK: an error occured but we can resolve it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(HomeScreen.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
 
     }
 }
